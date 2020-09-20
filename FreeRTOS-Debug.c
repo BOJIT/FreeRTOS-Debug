43f51e154e866b9b50a7edd7432de353b827fee9
BOJIT <35117353+BOJIT@users.noreply.github.com>
2020-09-20 20:57:19 +0100

    queue send handling is prototyped

--- FreeRTOS-Debug.c
+++ FreeRTOS-Debug.c
@@ -9,92 +9,127 @@
 
 #include "FreeRTOS-Debug.h"
 
-#include <stdio.h>
-
 #include <libopencm3/cm3/nvic.h>
+
+#include "queue.h"
 
 /*----------------------------- Global Variables -----------------------------*/
 
-/** @brief This message is added to the queue if there is only one space left */
-debug_t queue_full;
+#if DEBUG_LEVEL >= DEBUG_ERRORS
+    /**
+     * @brief This message is added to the queue if
+     * there is only one space left.
+     */
+    static debug_t queue_full;
+
+    /** @brief The queue itself */
+    static QueueHandle_t debug_queue;
+#endif /* DEBUG_LEVEL >= DEBUG_ERRORS */
 
 /*----------------------------- Private Functions ----------------------------*/
 
-/** 
- * @brief Internal function used to allocate memory for the error string.
- * @param length length of message passed in printf-style message.
-*/
-char* debug_pre_message(size_t length)
-{
-    return NULL;
-}
-
-/** 
- * @brief Internal function used add debug message to the queue.
- * @param debug_type debug message type - see Debug Types.
- * @param length length of message passed in printf-style message.
-*/
-void debug_post_message(debug_t debug)
-{
-
-}
-
-/**
- * @brief Task that handles actually sending the messages in a multi-threaded
- * environment.
- * @param send_func function pointer to a function that sends one char over
- * serial in a non-blocking manner.
-*/
-static void debug_handler(void *send_func)
-{
-    for(;;) {
-
+#if DEBUG_LEVEL >= DEBUG_ERRORS
+    /**
+     * @brief Internal function used add debug message to the queue.
+     * @param debug_type debug message type - see Debug Types.
+     * @param length length of message passed in printf-style message.
+     */
+    void debug_send_message(debug_t debug)
+    {
+        switch(uxQueueSpacesAvailable(debug_queue)) {
+            case 0:
+                break;
+            case 1:
+                {
+                    char full_message[] = "Queue Full!\n";
+                    queue_full.message = pvPortMalloc(sizeof(full_message));
+                    xQueueSend(debug_queue, &queue_full, 0);
+                    break;
+                }
+            default:
+                xQueueSend(debug_queue, &debug, 0);
+                break;
+        }
     }
-}
+
+    /**
+     * @brief Task that handles actually sending the messages in a multi-threaded
+     * environment.
+     * @param send_func function pointer to a function that sends one char over
+     * serial in a non-blocking manner.
+     */
+    static void debug_handler(void *send_func)
+    {
+        for(;;) {
+
+        }
+    }
+#endif /* DEBUG_LEVEL >= DEBUG_ERRORS */
 
 /*------------------------------ Public Functions ----------------------------*/
 
-/** 
+/**
  * @brief Initialise the queues and tasks associated with the debug handler.
- * This function should be called after the scheduler has started.
- * @param send_func function pointer to a function that sends one char over
- * serial in a non-blocking manner.
+ * @param queue_length defines the length of the message queue. While the a long
+ * message queue does not use up much memory, the dynamically allocated message
+ * strings that the queue items point to do. Keep as low as possible.
+ * @param init_func function pointer to a function that initialises the output
+ * means that send_func uses to do the logging.
+ * @param send_func function pointer to a function that sends one char in a
+ * non-blocking manner.
  * 
  * @retval handle of the task that was created to handle the debug messages.
  * This task will block after a character is sent, so should be unblocked with a
  * direct task notification in an ISR.
-*/
-TaskHandle_t debugInitialise(void (*send_func)(char), void (*init_func)(void))
+ */
+TaskHandle_t debugInitialise(size_t queue_length, void (*init_func)(void),
+                                                    void (*send_func)(char))
 {
-    /* Call passed initialisation function */
-    (*init_func)();
-
-    /* Create debug task and pass handle back to the user application */
     TaskHandle_t task_handle;
-    xTaskCreate(debug_handler, "debug", 350, (void *)send_func, 1, &task_handle);
+    #if DEBUG_LEVEL >= DEBUG_ERRORS
+        /* Call passed initialisation function */
+        (*init_func)();
+
+        /* Initialise message queue */
+        debug_queue = xQueueCreate(queue_length, sizeof(debug_t));
+
+        /* Create debug task and pass handle back to the user application */
+        xTaskCreate(debug_handler, "debug", 350, (void *)send_func,
+                                                            1, &task_handle);
+
+        /* Populate 'Queue Full' message partially */
+        queue_full.type = DEBUG_TYPE_ERROR;
+        queue_full.task_handle = task_handle;
+    #endif /* DEBUG_LEVEL >= DEBUG_ERRORS */
     return task_handle;
 }
 
-/** 
+/**
  * @brief Suspend all tasks and halt everything for debugging.
-*/
+ */
 void debugFreeze(void)
 {
-    vTaskSuspendAll();
+    #if DEBUG_LEVEL >= DEBUG_FULL
+        vTaskSuspendAll();
+    #endif /* DEBUG_LEVEL >= DEBUG_FULL */
 }
 
-/** 
+/**
  * @brief Suspend the calling task for debugging.
-*/
+ */
 void debugFreezeTask(void)
 {
-    vTaskSuspend((TaskHandle_t)NULL);
+    #if DEBUG_LEVEL >= DEBUG_ERRORS
+        vTaskSuspend((TaskHandle_t)NULL);
+    #endif /* DEBUG_LEVEL >= DEBUG_ERRORS */
 }
 
-/** 
+/**
  * @brief Reset the ARM CPU.
-*/
+ */
 void debugReset(void)
 {
-    reset_handler();
+    #if DEBUG_LEVEL >= DEBUG_MINIMAL
+        reset_handler();
+    #endif /* DEBUG_LEVEL >= DEBUG_MINIMAL */
 }
--- FreeRTOS-Debug.h
+++ FreeRTOS-Debug.h
@@ -41,67 +41,72 @@ typedef struct {
 
 /*----------------------------- Private Functions ----------------------------*/
 
-/** 
+/**
  * @brief Internal function used to allocate memory for the error string.
  * @param length length of message passed in printf-style message.
-*/
+ */
 char* debug_pre_message(size_t length);
 
-/** 
+/**
  * @brief Internal function used add debug message to the queue.
  * @param debug debug struct that is passed to the queue.
-*/
-void debug_post_message(debug_t debug);
+ */
+void debug_send_message(debug_t debug);
 
 /*------------------------------ Public Functions ----------------------------*/
 
-/** 
+/**
  * @brief Add debug message to output queue.
  * @param debug_type debug message type - see Debug Types.
  * @param message printf-style arguments.
-*/
+ */
 #ifdef DEBUG_LEVEL
-#if (DEBUG_LEVEL == DEBUG_ERRORS) || (DEBUG_LEVEL == DEBUG_WARNINGS) || \
-                                                (DEBUG_LEVEL == DEBUG_FULL)
+#if DEBUG_LEVEL >= DEBUG_ERRORS
     #define DEBUG_MESSAGE(debug_type, message) do { \
             debug_t debug; \
             debug.type = debug_type; \
-            debug.message = debug_pre_message(snprintf(NULL, 0, ESC(message)));\
+            debug.message = pvPortMalloc(snprintf(NULL, 0, ESC(message)));\
             sprintf(debug.message, ESC(message)); \
-            debug_post_message(debug); \
+            debug_send_message(debug); \
         } while(0)
 #else
     #define DEBUG_MESSAGE(debug_type, message)
-#endif
+#endif /* DEBUG_LEVEL >= DEBUG_ERRORS */
 #else
+    /* The existence of DEBUG_LEVEL is only checked here. */
     #error "No Debug Level Defined!!!"
 #endif /* DEBUG_LEVEL */
 
-/** 
+/**
  * @brief Initialise the queues and tasks associated with the debug handler.
- * This function should be called after the scheduler has started.
- * @param send_func function pointer to a function that sends one char over
- * serial in a non-blocking manner.
+ * @param queue_length defines the length of the message queue. While the a long
+ * message queue does not use up much memory, the dynamically allocated message
+ * strings that the queue items point to do. Keep as low as possible.
+ * @param init_func function pointer to a function that initialises the output
+ * means that send_func uses to do the logging.
+ * @param send_func function pointer to a function that sends one char in a
+ * non-blocking manner.
  * 
  * @retval handle of the task that was created to handle the debug messages.
  * This task will block after a character is sent, so should be unblocked with a
  * direct task notification in an ISR.
-*/
-TaskHandle_t debugInitialise(void (*send_func)(char), void (*init_func)(void));
+ */
+TaskHandle_t debugInitialise(size_t queue_length, void (*init_func)(void),
+                                                    void (*send_func)(char));
 
-/** 
+/**
  * @brief Suspend all tasks and halt everything for debugging.
-*/
+ */
 void debugFreeze(void);
 
-/** 
+/**
  * @brief Suspend the calling task for debugging.
-*/
+ */
 void debugFreezeTask(void);
 
-/** 
+/**
  * @brief Reset the ARM CPU.
-*/
+ */
 void debugReset(void);
 
-#endif /* __PORT__ */
+#endif /* __FREERTOS_DEBUG__ */
